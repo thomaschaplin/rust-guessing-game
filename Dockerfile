d7d0b13f51689aba9b39d357a828544f152bb6e2
Thomas Chaplin <thomaschaplin@outlook.com>
2023-06-08 23:54:30 +0100

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,47 @@
-FROM rust:latest as build
+####################################################################################################
+## Builder
+####################################################################################################
+FROM rust:latest AS builder
 
-USER root
+RUN rustup target add x86_64-unknown-linux-musl
+RUN apt update && apt install -y musl-tools musl-dev
+RUN update-ca-certificates
+
+# Create appuser
+ENV USER=rust-guessing-game
+ENV UID=10001
+
+RUN adduser \
+    --disabled-password \
+    --gecos "" \
+    --home "/nonexistent" \
+    --shell "/sbin/nologin" \
+    --no-create-home \
+    --uid "${UID}" \
+    "${USER}"
+
 
 WORKDIR /rust-guessing-game
 
-COPY ./Cargo.lock ./Cargo.lock
-COPY ./Cargo.toml ./Cargo.toml
-COPY ./src ./src
+COPY ./ .
 
-RUN cargo build --release
+RUN cargo build --target x86_64-unknown-linux-musl --release
 
-FROM rust:latest
+####################################################################################################
+## Final Image
+####################################################################################################
+FROM scratch
 
-COPY --from=build /rust-guessing-game/target/release/rust-guessing-game .
+# Import from builder.
+COPY --from=builder /etc/passwd /etc/passwd
+COPY --from=builder /etc/group /etc/group
 
-CMD ["./rust-guessing-game"]
+WORKDIR /rust-guessing-game
+
+# Copy our build
+COPY --from=builder /rust-guessing-game/target/x86_64-unknown-linux-musl/release/rust-guessing-game ./
+
+# Use an unprivileged user.
+USER rust-guessing-game:rust-guessing-game
+
+ENTRYPOINT ["./rust-guessing-game"]
--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,36 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn print_guess_the_number_message(min: u32, max: u32) -> () {
+fn print_guess_the_number_message(min: u32, max: u32) {
     println!("\nGuess the number between {} & {}\n", min, max);
 }
 
-fn print_input_your_guess_message() -> () {
+fn print_input_your_guess_message() {
     println!("Please input your guess.\n");
 }
 
-fn print_winning_message(answer: u32) -> () {
+fn print_winning_message(answer: u32) {
     println!("\n{} is correct, you win!\n", answer);
 }
 
-fn print_please_input_your_number_message(option: &str) -> () {
+fn print_please_input_your_number_message(option: &str) {
     println!("\nPlease input your {} number.\n", option);
 }
 
-fn print_too_small_message(guess: u32) -> () {
+fn print_too_small_message(guess: u32) {
     println!("\n{} is too small, try again!\n", guess);
 }
 
-fn print_too_large_message(guess: u32) -> () {
+fn print_too_large_message(guess: u32) {
     println!("\n{} is too large, try again!\n", guess);
 }
 
-fn panic_when_maximum_is_smaller_than_minimum(max: u32, min: u32) -> () {
+fn panic_when_maximum_is_smaller_than_minimum(max: u32, min: u32) {
     panic!("{} is smaller than {}, pick a larger number!\n", max, min);
 }
 
 fn generate_random_number_between(min: u32, max: u32) -> u32 {
-    return rand::thread_rng().gen_range(min..max);
+    rand::thread_rng().gen_range(min..max)
 }
 
 fn parse_guess_as_u32(guess: String) -> u32 {
@@ -43,7 +43,7 @@ fn parse_guess_as_u32(guess: String) -> u32 {
         ),
     };
 
-    return guess_as_u32;
+    guess_as_u32
 }
 
 fn get_user_defined_input_as_u32() -> u32 {
@@ -53,25 +53,25 @@ fn get_user_defined_input_as_u32() -> u32 {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess_as_u32 = parse_guess_as_u32(guess);
+    
 
-    return guess_as_u32;
+    parse_guess_as_u32(guess)
 }
 
 fn get_user_defined_input(option: &str) -> u32 {
     print_please_input_your_number_message(option);
 
-    return get_user_defined_input_as_u32();
+    get_user_defined_input_as_u32()
 }
 
 fn get_minimum_and_maximum_inputs() -> [u32; 2] {
     let min = get_user_defined_input("minimum");
     let max = get_user_defined_input("maximum");
 
-    return [min, max];
+    [min, max]
 }
 
-fn validate_maximum_input(min: u32, max: u32) -> () {
+fn validate_maximum_input(min: u32, max: u32) {
     match min.cmp(&max) {
         Ordering::Less => {
             game(min, max);
@@ -83,7 +83,7 @@ fn validate_maximum_input(min: u32, max: u32) -> () {
     }
 }
 
-fn does_guess_equal_answer_loop(answer: u32) -> () {
+fn does_guess_equal_answer_loop(answer: u32) {
     loop {
         let mut guess = String::new();
 
@@ -107,14 +107,14 @@ fn does_guess_equal_answer_loop(answer: u32) -> () {
     }
 }
 
-fn game(min: u32, max: u32) -> () {
+fn game(min: u32, max: u32) {
     print_guess_the_number_message(min, max);
     let random_number = generate_random_number_between(min, max);
     print_input_your_guess_message();
     does_guess_equal_answer_loop(random_number);
 }
 
-fn main() -> () {
+fn main() {
     let [min, max] = get_minimum_and_maximum_inputs();
     validate_maximum_input(min, max)
 }
